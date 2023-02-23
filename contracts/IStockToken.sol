995c449b8d8f05a4e33f5a764b008ef4fdf2b274
ThinkerTechnology <defichainnfts@gmx.de>
2023-02-23 14:35:29 +0530

    remove mint and burn func

--- contracts/IStockToken.sol
+++ contracts/IStockToken.sol
@@ -1,17 +1,15 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.4;
+pragma solidity 0.8.17;
 
 interface IStockToken {
-    /// @dev   Function to mint the stock token into the user's wallet; called by the Reflection wallet when the user
-    ///        buys a stock token on the Reflection platform
-    /// @param to The user's wallet address into which the StockToken will be minted
-    /// @param _amount The number of StockTokens that will be minted into the user's wallet
+    /// @dev    Function to mint the stock token into the user's wallet; called when the user buys a stock token on the Reflection platform
+    /// @param to (address)
+    /// @param _amount (uint256)
     function mint(address to, uint256 _amount) external;
 
-    /// @dev   Function to burn the stock token out of the user's wallet; called by the Reflection wallet when the user
-    ///        sells a stock token on the Reflection platform
-    /// @param from The user's wallet address from which the StockTokens will be burned
-    /// @param _amount The number of StockTokens that will be burned out of the user's wallet
+    /// @dev    Function to burn the stock token out of the user's wallet; called when the user sells a stock token on the Reflection platform
+    /// @param from (address)
+    /// @param _amount (uint256)
     function burn(address from, uint256 _amount) external;
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,5 +4,4 @@ var BUSD=artifacts.require("BUSD");
 module.exports = async(deployer)=> {
     deployer.deploy(RUSD,"0xe5840CbA5f24c64db82f8f13d521811A7a5ad4D8");
     deployer.deploy(stockToken,"stock Token","ST","0xe5840CbA5f24c64db82f8f13d521811A7a5ad4D8","0x3b07836A939C7135c00FFc3BBD80609f30BC90E3");
-    deployer.deploy(BUSD,"0xe5840CbA5f24c64db82f8f13d521811A7a5ad4D8");
 };
